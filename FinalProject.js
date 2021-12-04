3bfe680281c27ad5f74187a45a4efa62637b0b32
Jimmy <jimmy.kha@mytwu.ca>
2021-12-04 13:13:21 -0800

    file reading

--- FinalProject.js
+++ FinalProject.js
@@ -375,6 +375,18 @@ window.onload = function init() {
     render();
 }
 
+document.getElementById('inputfile')
+.addEventListener('change', function() {
+  
+var fr=new FileReader();
+fr.onload=function(){
+    document.getElementById('output')
+            .textContent=fr.result;
+}
+  
+fr.readAsText(this.files[0]);
+})
+
 //function for setting the texture
 
 //render function
